e1585caaf32cd7b9ffa26bdd532f082a436e7973
SimonMoloney <erzaflame@gmail.com>
2018-10-13 15:56:08 +0100

    Update Ian_Target_detection_with_Switch/Ian_Target_detection_with_Switch.ino

---
+++ Ian_Target_detection_with_Switch/Ian_Target_detection_with_Switch.ino
@@ -0,0 +1,219 @@
+//
+
+//bool value = falsee // true false statement
+//void function () {} // for grouping a seet of statements
+// int function () {} // returns an int from function
+// bool function ()
+// &&   // LOGICAL AND
+// !    // LOGICAL NOT
+// ||   // LOGICAL OR
+
+
+// RoboSlam Target Finding
+// Written by Ciaran Gibson and Simon Moloney
+//
+
+//
+// HC-SR Rangfinder test
+// Written by Simon Moloney Ref Ted Burke 9/10/2018
+//
+//---
+int target_hit = 0;
+bool spin_left = false;
+bool target_acquired = false;
+int  distances [100];
+int  index;
+int  max_instances = 10;
+//--
+
+// The setup routine runs once when the power is switched on.
+void setup()  
+{
+  // Digital output for LED 
+  pinMode(2, OUTPUT); //LED
+
+  // Digital output for Wheels on H-bridge
+  pinMode(3, OUTPUT); //wheel 1 (Forward)Left
+  pinMode(4, OUTPUT); //wheel 1 (Reverse)Left
+  pinMode(7, OUTPUT); //wheel 2 (Reverse)Right
+  pinMode(8, OUTPUT); //wheel 2 (Forward)Right
+
+  //Digital Outputs for Rangefinder
+  pinMode(5, OUTPUT); // rangefinder trigger
+  
+
+  Serial.begin(9600);
+}
+
+int calculate_distance(int duration) {
+  return 0.5 * 1e-6 * duration * 340.0*1.41; // 1.41 was for distance correction
+}
+
+function smooth(int[] distances) {
+  // sort array
+  // return middle value of array - max_instaces / 2
+}
+
+void loop()
+{
+  int duration, cycles;
+  double distance;
+  int has_hit, median_distance;
+
+
+
+   
+  // Send trigger pulse
+  digitalWrite(5, HIGH);            //Trig Pulse
+  delayMicroseconds(20);
+  digitalWrite(5, LOW);
+ 
+  // Wait for start of echo pulse     
+  while(digitalRead(6) == 0);
+ 
+  // No measure duration of echo pulse
+  duration = 0;
+  while(digitalRead(6) == 1)
+  {
+    duration = duration + 10;
+    delayMicroseconds(10);
+  }
+ 
+  distance = calculate_distance(duration);
+  distances[index] = distance;
+  index++;
+
+  median_distance = smooth(distances);
+  if (index >= max_instances) {
+    index = 0;
+  }
+  //Want to find median of distance
+  
+  Serial.print (distance);
+  Serial.println (" m");
+
+//---
+
+ 
+  has_hit = digitalRead(9); // switch input
+   
+  if (has_hit == 1)
+
+  {
+   
+  target_hit = 1 - target_hit;
+
+    delay(500);
+    
+    //Reverse
+    digitalWrite(3, LOW);       //wheel 1 (Reverse)Left
+    digitalWrite(4, HIGH);
+    digitalWrite(7, HIGH);      //wheel 2 (Reverse)Right
+    digitalWrite(8, LOW);
+  
+
+  
+  } else if (median_distance < 0.5)
+  {
+    target_aquired = true;
+    digitalWrite(2, HIGH);
+
+    delay(500);
+    
+    digitalWrite(3, HIGH);     //wheel 1 (Forward)Left
+    digitalWrite(4, LOW);
+    digitalWrite(7, LOW);      //wheel 2 (Forward)Right
+    digitalWrite(8, HIGH);
+    
+  }
+  else
+  {
+    if (distance > 0.5 && target_acquired) {
+      target_acquired = false;
+      spin_left = !spin_left;  
+    }
+    if (!spin_left) {
+      digitalWrite(2, LOW);
+  
+      turn_right();
+    } else {
+      digitalWrite(2, HIGH);
+  
+      digitalWrite(3, HIGH);      //wheel 1 (Reverse)Left
+      digitalWrite(4, LOW);
+      digitalWrite(7, HIGH);      //wheel 2 (Forward)Right
+      digitalWrite(8, LOW);
+    }
+ 
+  } 
+}
+
+void turn_right(){
+  digitalWrite(3, LOW);      //wheel 1 (Reverse)Left
+  digitalWrite(4, HIGH);
+  digitalWrite(7, LOW);      //wheel 2 (Forward)Right
+  digitalWrite(8, HIGH);
+}
+
+//- Searching motor code
+
+//Wheel 1 Forward 5 secs
+  //digitalWrite(3, HIGH);      //wheel 1 (Forward)
+  //digitalWrite(4, LOW);
+  //digitalWrite(7, LOW);       //wheel 2 (Reverse)
+  //digitalWrite(8, HIGH);
+  //delay(5000);
+//- Object found move forward 
+  //digitalWrite(3, HIGH);      //wheel 1 (Forward)
+  //digitalWrite(4, LOW);
+  //digitalWrite(7, HIGH);      //wheel 2 (Forward)
+ // digitalWrite(8, LOW);
+//  delay(5000);
+//-
+
+
+
+
+
+  //Wheel 1 Forward 5 secs
+//  digitalWrite(3, HIGH);      //wheel 1 (Forward)
+//  digitalWrite(4, LOW);   
+    //Wheel 2 Stop
+//  digitalWrite(7, LOW);     
+//  digitalWrite(8, LOW);
+//  delay(5000);           
+
+  //Wheel 2 Forward 5 secs
+  //digitalWrite(7, HIGH);      //wheel 2 (Forward)
+  //digitalWrite(8, LOW);
+    //Wheel 1 Stop
+  //digitalWrite(3, LOW);      //wheel 1 (Forward)
+  //digitalWrite(4, LOW); 
+  //delay(5000);     
+
+   // Brake
+  //Wheel 1 Stop
+  //digitalWrite(3, LOW);      //wheel 1 (Forward)
+  //digitalWrite(4, LOW);   
+          
+  //Wheel 2 Stop
+  //digitalWrite(7, LOW);      //wheel 2 (Forward)
+  //digitalWrite(8, LOW);
+  //delay(5000);     
+
+  
+   //Both Forward 5 secs
+  //digitalWrite(3, HIGH);      //wheel 1 (Forward)
+  //digitalWrite(4, LOW); 
+  //digitalWrite(7, HIGH);      //wheel 2 (Forward)
+  //digitalWrite(8, LOW);
+  //delay(5000);           
+
+    //Both Reverse 5 secs
+  //digitalWrite(3, LOW);       //wheel 2 (Reverse
+  //digitalWrite(4, HIGH); 
+  //digitalWrite(7, LOW);       //wheel 2 (Reverse)
+  //digitalWrite(8, HIGH);
+  //delay(5000);           
+
+//}
